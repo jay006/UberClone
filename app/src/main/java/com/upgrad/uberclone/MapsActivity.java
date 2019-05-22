9f055e983bcb54b9738ca3ce8f90fb6901701d61
Shriom <shriomtripathi33@gmail.com>
2019-05-23 03:52:11 +0530

    Mapactivity UI done

--- app/src/main/java/com/upgrad/uberclone/MapsActivity.java
+++ app/src/main/java/com/upgrad/uberclone/MapsActivity.java
@@ -1,41 +1,21 @@
 package com.upgrad.uberclone;
 
-import android.content.DialogInterface;
-import android.content.pm.PackageManager;
-import android.location.Location;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.places.GeoDataClient;
-import com.google.android.gms.location.places.PlaceDetectionClient;
-import com.google.android.gms.location.places.PlaceLikelihood;
-import com.google.android.gms.location.places.PlaceLikelihoodBufferResponse;
-import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.upgrad.uberclone.R;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback , MapListener{
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, MapListener{
 
     private GoogleMap map;
 
@@ -68,6 +48,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
+    @SuppressLint("MissingPermission")
     @Override
     public void onMapReady(GoogleMap map) {
         this.map = map;
@@ -114,5 +95,10 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
             map.getUiSettings().setMyLocationButtonEnabled(false);
         }
+    }
+
+    public void openNavigation(View view) {
+        //TODO open navigation
+
     }
 }
