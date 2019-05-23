34b03950b9b9ce8bb695ac09639d9ccf44e18024
Shriom <shriomtripathi33@gmail.com>
2019-05-23 11:57:46 +0530

    Search Activity finished

--- app/src/main/java/com/upgrad/uberclone/SearchActivity.java
+++ app/src/main/java/com/upgrad/uberclone/SearchActivity.java
@@ -1,17 +1,36 @@
 package com.upgrad.uberclone;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.location.Address;
 import android.location.Geocoder;
+import android.os.Parcel;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.LocationBias;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.RectangularBounds;
+import com.google.android.libraries.places.api.model.TypeFilter;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 
@@ -19,11 +38,14 @@ public class SearchActivity extends AppCompatActivity {
 
     private TextView sourceTV;
     private EditText destinationET;
-
+    private RecyclerView searchList;
     private LinearLayout moreContainer;
 
     private double latitude = 0.00;
     private double longitude = 0.00;
+
+    private List<Address> addresses = new ArrayList<>();
+    private static final String TAG = SearchActivity.class.getCanonicalName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +57,58 @@ public class SearchActivity extends AppCompatActivity {
         longitude = intent.getDoubleExtra(Constants.LONGITUDE, 0);
 
         sourceTV = findViewById(R.id.sourceTV);
-        destinationET = findViewById(R.id.destinationET);
+        //destinationET = findViewById(R.id.destinationET);
         moreContainer = findViewById(R.id.moreContainer);
+        searchList = findViewById(R.id.searchList);
 
         getNameFromCoordinate();
+        initListener();
 
+    }
+
+    private void initListener() {
+
+        Places.initialize(this, getResources().getString(R.string.google_maps_key), Locale.ENGLISH);
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment) getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME));
+        autocompleteFragment.setHint("Where to?");
+        autocompleteFragment.setCountry("IN");
+        autocompleteFragment.setTypeFilter(TypeFilter.ADDRESS);
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
+                getListOfAddress(place.getName());
+            }
+
+            @Override
+            public void onError(Status status) {
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
+    }
+
+    private void getListOfAddress(String query) {
+
+        addresses.clear();
+        Geocoder geocoder = new Geocoder(this);
+
+        try {
+            addresses = geocoder.getFromLocationName(query, 1);
+        } catch (IOException e) {
+            Log.d(SearchActivity.class.getCanonicalName(), e.getMessage());
+        }
+
+        if (addresses != null && addresses.size() > 0) {
+
+            Intent resultIntent = new Intent();
+            resultIntent.putExtra(Constants.LATITUDE, addresses.get(0).getLatitude());
+            resultIntent.putExtra(Constants.LONGITUDE, addresses.get(0).getLongitude());
+            setResult(Constants.SEARCH_REQUEST_CODE, resultIntent);
+            finish();
+        }
 
     }
 
